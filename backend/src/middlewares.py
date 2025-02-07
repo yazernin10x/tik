a2c86526a74adbedeb695f3c86ad7bc839b6468b
Ali YARO <aliyaro104@gmail.com>
2025-02-07 15:34:22 +0100

    Replace function-based middleware with class-based middleware.

--- backend/src/middlewares.py
+++ backend/src/middlewares.py
@@ -1,12 +1,14 @@
 from fastapi import Request
 from src.backend.config import SESSION_LOCAL
+from starlette.middleware.base import BaseHTTPMiddleware
 
 
-async def db_session(request: Request, call_next):
-    db = SESSION_LOCAL()
-    request.state.db = db
-    try:
-        response = await call_next(request)
-    finally:
-        db.close()
-    return response
+class DBSessionMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        db = SESSION_LOCAL()
+        request.state.db = db
+        try:
+            response = await call_next(request)
+        finally:
+            db.close()
+        return response
