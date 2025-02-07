085887734ea1053a314c75379ad731699a2dc084
Ali YARO <aliyaro104@gmail.com>
2025-02-07 15:32:43 +0100

    Implement custom exceptions

---
+++ backend/src/exceptions.py
@@ -0,0 +1,15 @@
+from fastapi import status, HTTPException
+
+
+class NotFoundException(HTTPException):
+    def __init__(self, detail: str = "Resource not found"):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+
+class CredentialsException(HTTPException):
+    def __init__(self, detail: str = "Could not validate credentials"):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=detail,
+            headers={"WWW-Authenticate": "Bearer"},
+        )
